1715e3544eb4cd1428bfc11b85c95235205ce469
asmejia1993 <asamu1993@gmail.com>
2023-02-09 22:04:17 -0600

    chore: spring boot 3 + jwt + spring security

---
+++ src/main/java/com/asmejia1993/security/auth/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.asmejia1993.security.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+}
---
+++ src/main/java/com/asmejia1993/security/user/UserRepository.java
@@ -0,0 +1,12 @@
+package com.asmejia1993.security.user;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+}
